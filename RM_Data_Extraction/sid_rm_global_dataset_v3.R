b6778891cb019374e8c1ff972e6cbeeb490a1c18
Femi Akinmade <qlx6@cdc.gov>
2022-03-30 07:17:02 -0400

    Commented a write line

--- RM_Data_Extraction/sid_rm_global_dataset_v3.R
+++ RM_Data_Extraction/sid_rm_global_dataset_v3.R
@@ -15,5 +15,5 @@ df.list <- lapply(rm.list, read_excel)  # Read in multiple files
 global_df <- bind_rows(df.list)
 
 setwd("C:/Users/qlx6/OneDrive - CDC/general dynamics - icpi/clusters.teams.workgroups/clusters/hss-kp/dm_sid/forMerge/pre_final_output/dataset_rm") # Set where you want the files should be saved
-openxlsx::write.xlsx(global_df, file = paste("RM_Global_Extract_",date, "_Time_09-30.xlsx", sep = ""), keepNA = T, asTable = TRUE)
-write.csv(global_df, file = "C:/Users/qlx6/OneDrive - CDC/general dynamics - icpi/clusters.teams.workgroups/clusters/hss-kp/dm_sid/forMerge/pre_final_output/dataset_rm/RM_Global_Dataset_09-30.csv")
+openxlsx::write.xlsx(global_df, file = paste("RM_Global_Extract_",date, "_Time_0500hrs.xlsx", sep = ""), keepNA = T, asTable = TRUE)
+#write.csv(global_df, file = "C:/Users/qlx6/OneDrive - CDC/general dynamics - icpi/clusters.teams.workgroups/clusters/hss-kp/dm_sid/forMerge/pre_final_output/dataset_rm/RM_Global_Dataset_09-30.csv")
